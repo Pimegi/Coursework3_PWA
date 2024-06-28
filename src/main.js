6e8800c56c091b75732adec55066aa0493ef5f2a
Pimegi <pimegi@gmail.com>
2024-06-28 06:23:57 +0400

    a

--- src/main.js
+++ src/main.js
@@ -4,3 +4,13 @@ import '../public/styles.css'
 
 
 createApp(App).mount('#app')
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function () {
+        navigator.serviceWorker.register('/service-worker.js').then(function (registration) {
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function (err) {
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
