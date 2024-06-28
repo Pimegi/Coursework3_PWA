3358943223d67cda6e94748a4b16fcfebc8d79ee
Pimegi <pimegi@gmail.com>
2024-06-28 20:47:38 +0400

    changes

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var cacheName = 'petstore-v1';
+var cacheName = 'afterschoolclasses-v1';
 var cacheFiles = [
     './index.html',
     './favicon.ico',
@@ -18,22 +18,24 @@ var cacheFiles = [
 ];
 
 self.addEventListener('install', (e) => {
-    console.log('Service Worker: Installing...');
+    console.log('service-worker')
     e.waitUntil(
-        caches.open(cacheName).then((cache) => {
+        caches.open(cacheName).then((cache) =>{
             console.log('[Service Worker] Caching all the files');
-            return cache.addAll(cacheFiles);
+            return cache.addAll(cacheFiles)
         })
     );
 });
 
-self.addEventListener('fetch', (e) => {
+self.addEventListener('fetch', function(e) {
     e.respondWith(
-        caches.match(e.request).then((response) => {
-            return response || fetch(e.request).then((fetchResponse) => {
-                return caches.open(cacheName).then((cache) => {
-                    cache.put(e.request, fetchResponse.clone());
-                    return fetchResponse;
+        caches.match(e.request).then(function(r) {
+            // Download the file if it is not in the cache
+            return r || fetch(e.request).then(function(response) {
+                // Add the new file to cache
+                return caches.open(cacheName).then(function(cache) {
+                    cache.put(e.request, response.clone());
+                    return response;
                 });
             });
         })
