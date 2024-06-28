ffef958438d4ae14f82db40af5eef8c5b0e16cd6
Pimegi <pimegi@gmail.com>
2024-06-28 05:27:32 +0400

    installed pwa

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,17 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/Coursework3_PWA/" : "/",
+  publicPath: process.env.NODE_ENV === 'production' ? '/Coursework3_PWA/' : '/',
+  pwa: {
+    name: 'PWA App',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    manifestOptions: {
+      
+    },
+    workboxOptions: {
+      skipWaiting: true,
+      clientsClaim: true,
+    },
+  },
 };
